5c59e541ef2b8bd9c5cfe9d929c5c6e2c2b14279
Mason ma <MAMA14@oocl.com>
2024-12-02 11:55:51 +0800

    feat:init project

---
+++ src/main/java/oocl/springboottodosbackend/SpringbootTodosBackendApplication.java
@@ -0,0 +1,13 @@
+package oocl.springboottodosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodosBackendApplication.class, args);
+	}
+
+}
